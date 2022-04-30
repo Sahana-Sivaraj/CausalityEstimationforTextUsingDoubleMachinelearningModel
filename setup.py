fb7c08b48f70282934495f18300e58608247c14f
sahana-at-014733626787 <sivarajsahana@gmail.com>
2022-04-30 14:45:29 +0530

    minor changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ReviewsCasualEstimation',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='sivaraj',
+    author_email='sivarajsahana@gmail.com',
+    description=''
+)
